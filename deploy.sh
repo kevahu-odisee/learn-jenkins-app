800c21788760ec11436cda077c0b6450036da6c7
Kellian Van Hullebusch <kellian.vanhullebusch@student.odisee.be>
2024-12-11 20:57:16 +0100

    Quick test prob won't work

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-find build/ -name "*.*" | sed "s/build\///" | while read FILE; do wget --post-file="build/$FILE" -qO dev/null --header "Token: 22adee63-83f2-45ab-a432-412bf21b13d9" "https://test.kevahu.net/$FILE"; done
+find build/ -name "*.*" | sed "s/build\///" | while read FILE; do wget --post-file="build/$FILE" -qO dev/null --header "Token: $WEB_DEPLOY_TOKEN" "https://test.kevahu.net/$FILE"; done
